913665bd1a77ba59d3f8664075c0c80341d41329
Fernabim <109820905+FernandaNav@users.noreply.github.com>
2024-07-31 10:19:49 -0600

    Cambios en la clase de datos de receta e implementación de switch

---
+++ DatosReceta.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace RECETAS
+{
+    internal class DatosReceta
+    {
+        public string Nombre {  get; set; }
+        public string Descripcion { get; set; }
+        public string Ingredientes { get; set; }
+
+        public List<DatosReceta> IngresarReceta = new List<DatosReceta>();
+
+        public DatosReceta(string nombre, string descripcion, string ingredientes)
+        {
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Ingredientes = ingredientes;
+        }
+
+        public string NombreReceta()
+        {
+            string nombre;
+            Console.Write("Ingresa el nombre de la receta: ");
+            nombre = Console.ReadLine();
+            return nombre;
+        }
+
+        public string DescripcionReceta()
+        {
+            string descripcion;
+            Console.Write("Ingresa la descripción de la receta: ");
+            descripcion = Console.ReadLine();
+            return descripcion;
+        }
+        public string IngredientesReceta()
+        {
+            string ingredientes;
+            Console.WriteLine("Ingresa los ingredientes de la receta separados por comas: ");
+            ingredientes = Console.ReadLine();
+            return ingredientes;
+        }
+
+        public void AgregarReceta()
+        {
+            string nombre = NombreReceta();
+            string descripcion = DescripcionReceta();
+            string ingredientes = IngredientesReceta();
+
+            DatosReceta nuevaReceta = new DatosReceta(nombre, descripcion, ingredientes);
+            IngresarReceta.Add(nuevaReceta);
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Drawing;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int opcion=0;
+        do
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("     R  E  C  E T  A  S");
+            Console.WriteLine("-----------------------------"); Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("1. Agregar receta. ");
+            Console.WriteLine("2. Buscar receta. ");
+            Console.WriteLine("3. Mostrar receta. ");
+            Console.WriteLine("4. Salir. ");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Elige la opción: ");
+            try
+            {
+
+            }
+            catch (FormatException)
+            {
+                Console.ForegroundColor= ConsoleColor.Red;
+                Console.WriteLine("Error de formato. Intenta de nuevo");
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("Puilsa cualquier tecla para continuar...");
+                Console.ReadKey();
+            }
+        } while (opcion != 4);
+    }
+}
