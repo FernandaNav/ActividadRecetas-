c011be2f9c039f30968af172bd0a9b8f574bd06e
Fernabim <109820905+FernandaNav@users.noreply.github.com>
2024-08-01 02:35:21 -0600

    Terminado método para mostrar, iniciado el método buscar

--- DatosReceta.cs
+++ DatosReceta.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,18 +8,21 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace RECETAS
 {
-    internal class DatosReceta
+    public class DatosReceta
     {
+        static int siguienteId = 0;
+        public int IdReceta {get; set;}
         public string Nombre {  get; set; }
-        public string Descripcion { get; set; }
+        public string Procedimiento { get; set; }
         public string Ingredientes { get; set; }
 
         public List<DatosReceta> IngresarReceta = new List<DatosReceta>();
 
-        public DatosReceta(string nombre, string descripcion, string ingredientes)
+        public DatosReceta(string nombre, string procedimiento, string ingredientes)
         {
+            IdReceta = siguienteId++;
             Nombre = nombre;
-            Descripcion = descripcion;
+            Procedimiento = procedimiento;
             Ingredientes = ingredientes;
         }
 
@@ -30,12 +34,12 @@ namespace RECETAS
             return nombre;
         }
 
-        public string DescripcionReceta()
+        public string ProcedimientoReceta()
         {
-            string descripcion;
+            string procedimiento;
             Console.Write("Ingresa la descripción de la receta: ");
-            descripcion = Console.ReadLine();
-            return descripcion;
+            procedimiento = Console.ReadLine();
+            return procedimiento;
         }
         public string IngredientesReceta()
         {
@@ -48,11 +52,62 @@ namespace RECETAS
         public void AgregarReceta()
         {
             string nombre = NombreReceta();
-            string descripcion = DescripcionReceta();
+            string descripcion = ProcedimientoReceta();
             string ingredientes = IngredientesReceta();
-
             DatosReceta nuevaReceta = new DatosReceta(nombre, descripcion, ingredientes);
             IngresarReceta.Add(nuevaReceta);
+        }
+
+        public void BuscarReceta()
+        {
+ 
+            bool nombreEncontrado = false;
+            string nombreParaBuscar;
+            do
+            {
+                Console.Write("Ingresa el nombre de la receta que quieras buscar: ");
+                nombreParaBuscar = Console.ReadLine();
+
+                foreach (var receta in IngresarReceta)
+                {
+                    if (nombreParaBuscar.ToLower() == receta.Nombre.ToLower())
+                    {
+                        nombreEncontrado = true;
+                        Console.WriteLine("");
+                        Console.WriteLine($"Receta #{receta.IdReceta}");
+                        Console.WriteLine($"Nombre: {receta.Nombre}");
+                        Console.WriteLine($"Procedimiento: {receta.Procedimiento}");
+                        Console.WriteLine($"Ingredientes: {receta.Ingredientes}");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor=ConsoleColor.DarkRed;
+                        Console.WriteLine("No existe una receta con ese nombre, intenta de nuevo");
+                        Console.WriteLine();
+                        Console.ResetColor();
+                        
+                    }
+                }
+            } while (nombreEncontrado == false);
+        }
+
+        public void MostrarRecetas()
+        {
+            foreach(var receta in IngresarReceta)
+            {
+                Console.WriteLine(""); Console.ResetColor();
+                Console.WriteLine($"Receta #{receta.IdReceta}");
+                Console.WriteLine($"Nombre: {receta.Nombre}");
+                Console.WriteLine($"Procedimiento: {receta.Procedimiento}");
+                Console.WriteLine($"Ingredientes: {receta.Ingredientes}");
+            }
+        }
+
+        public void MensajeParaContinuar()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("Presiona cualquier tecla para continuar...");
+            Console.ResetColor(); Console.ReadKey(); Console.Clear();
         }
     }
 
--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
-﻿using System.Drawing;
+﻿using RECETAS;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        int opcion=0;
+        int opcion = 0;
+        string nombre = "", descripcion = "", ingredientes = "";
+        DatosReceta nuevaReceta = new DatosReceta(nombre, descripcion, ingredientes);
         do
         {
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("-----------------------------");
             Console.WriteLine("     R  E  C  E T  A  S");
             Console.WriteLine("-----------------------------"); Console.ResetColor();
@@ -18,20 +20,55 @@ class Program
             Console.WriteLine("3. Mostrar receta. ");
             Console.WriteLine("4. Salir. ");
             Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("Elige la opción: ");
             try
             {
+                opcion = Convert.ToInt32(Console.ReadLine());
+                switch(opcion)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("--------------");
+                        Console.WriteLine("AGREGAR RECETA"); Console.WriteLine("--------------");
+                        Console.ResetColor();
+                        nuevaReceta.AgregarReceta();
+                        nuevaReceta.MensajeParaContinuar();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("--------------");
+                        Console.WriteLine("BUSCAR RECETA"); Console.WriteLine("--------------");
+                        Console.ResetColor();
+                        nuevaReceta.BuscarReceta();
+                        nuevaReceta.MensajeParaContinuar();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("--------------");
+                        Console.WriteLine("MOSTRAR RECETA"); Console.WriteLine("--------------");
+                        nuevaReceta.MostrarRecetas();
+                        nuevaReceta.MensajeParaContinuar();
+                        break;
+                    case 4:
+                        Console.Clear(); 
+                        Console.WriteLine("-----------------"); Console.ForegroundColor= ConsoleColor.Green;
+                        Console.WriteLine(" Ten un buen día");
+                        Console.WriteLine("-----------------");
+                        break;
+                    default:
 
+                        nuevaReceta.MensajeParaContinuar();
+                        break;
+                }
             }
             catch (FormatException)
             {
-                Console.ForegroundColor= ConsoleColor.Red;
-                Console.WriteLine("Error de formato. Intenta de nuevo");
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-                Console.WriteLine("Puilsa cualquier tecla para continuar...");
-                Console.ReadKey();
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Error en el formato ingresado. Intenta de nuevo.");
+                nuevaReceta.MensajeParaContinuar();
             }
+            Console.ResetColor();
         } while (opcion != 4);
     }
 }
